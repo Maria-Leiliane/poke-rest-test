2a788b5a93e988a882e93ca5681baba51754c7ce
llsMaria <maria.lobo@asapcred.com.br>
2025-06-16 21:33:48 -0300

    fix: custn wlements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<ion-app><ion-router-outlet></ion-router-outlet></ion-app>',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {}
